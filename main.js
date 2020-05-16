4685fc441f8e9fa9d04699a373e1365e66b54ffa
hottunasandwich <55018126+hottunasandwich@users.noreply.github.com>
2020-05-16 21:18:50 +0430

    added build configs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 ///////////////////////////////////////////////////////////////////////////////
-function createWindow() {
+async function createWindow() {
   // Create the browser window.
   let win = new BrowserWindow({
     // alwaysOnTop: process.env === 'development'? false : true,
@@ -15,28 +15,46 @@ function createWindow() {
   win.setMenuBarVisibility(false)
 
   const SerialPort = require('serialport')
-  const ByteLength = require('@serialport/parser-byte-length')
-  const port = new SerialPort('COM3', {
-    stopBits: 2,
-    parity: 'even'
+  SerialPort.list().then((serial) => {
+    if (serial.length === 0) {
+      console.log('No serial found')
+    } else {
+      const ByteLength = require('@serialport/parser-byte-length')
+      const port = new SerialPort(serial[0].path, {
+        stopBits: 2,
+        parity: 'even'
+      })
+      port.on('error', (err) => {
+        if (err.disconnected === true) {
+          console.log('Port Disconnected')
+        } else {
+          console.log(err)
+        }
+      })
+
+      var i = 0
+      const parser = port.pipe(new ByteLength({ length: 5 }))
+      parser.on('data', (chunk) => {
+        i++;
+        if (i === 10) {
+          var buffer = new Buffer(chunk)
+          var data = buffer.toJSON().data
+          var binaryweight = dec2bin(data[2]) + dec2bin(data[3]) + dec2bin(data[4])
+          var weight = parseInt(binaryweight, 2)
+          win.webContents.send('weight', weight)
+          i = 0
+        }
+      })
+    }
+  }).catch((err) => {
+    console.log(err)
   })
 
-  var i = 0
-  const parser = port.pipe(new ByteLength({ length: 5 }))
-  parser.on('data', (chunk) => {
-    i++;
-    if (i === 10) {
-      var buffer = new Buffer(chunk)
-      var data = buffer.toJSON().data
-      var binaryweight = dec2bin(data[2]) + dec2bin(data[3]) + dec2bin(data[4])
-      var weight = parseInt(binaryweight, 2)
-      win.webContents.send('weight', weight)
-      i = 0
-    }
-  })
 }
 
 app.whenReady().then(createWindow)
+
+app.allowRendererProcessReuse = true
 
 function dec2bin(dec) {
   return (dec >>> 0).toString(2).padStart(7, '0');
